26a5479acf64114a129beb56d2ad537fa58dda3e
lizbeth <lizbethmaganalml@gmail.com>
2018-06-05 22:09:04 -0500

    add cotizador

---
+++ objetos/main.js
@@ -0,0 +1,12 @@
+let obj = {
+  nombre: "Angelo",
+  apellido: "Luna"
+  direcciones: [0, 12, 4, 5],
+  otra: {
+    otracosa1: "cosa1",
+    otracosa2: "cosa2",
+  },
+
+}
+
+console.log(obj.nombre);
